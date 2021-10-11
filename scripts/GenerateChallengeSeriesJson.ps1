95db806bebf3652f6d32ef3a7b7b431d6a81454e
Paul Wheeler <paul@invioinc.com>
2021-10-11 01:51:32 -1000

    Added DEMRR Results. Standaradized JSON ordering.

--- scripts/GenerateChallengeSeriesJson.ps1
+++ scripts/GenerateChallengeSeriesJson.ps1
@@ -1,4 +1,71 @@
+using namespace Newtonsoft.Json
+
 param ($ChallengeResultsCsv)
+
+[JsonObject()]
+class AthleteResult {
+  [JsonProperty(Order = 4)][String]$Name
+  [JsonProperty(Order = 3, NullValueHandling = [NullValueHandling]::Ignore)][Nullable[Int32]]$Place
+  [JsonProperty(Order = 2)][Int32]$Points
+  [JsonProperty(Order = 1, NullValueHandling = [NullValueHandling]::Ignore)][String]$Time
+
+  AthleteResult([String]$AthleteName, [Int32]$ResultPoints, [Nullable[Int32]]$ResultPlace, [String]$ResultTime) {
+    $this.Name = $AthleteName;
+    $this.Place = $ResultPlace;
+    $this.Points = $ResultPoints;
+    if (-not [String]::IsNullOrEmpty($ResultTime)) {
+      $this.Time = $ResultTime
+    }
+  }
+}
+
+[JsonObject()]
+class CategoryResults {
+  [JsonProperty(Order = 2)][String]$Name
+  [JsonProperty(Order = 1)][AthleteResult[]]$Results
+
+  CategoryResults([String]$CategoryName, [AthleteResult[]]$CategoryResults) {
+    $this.Name = $CategoryName
+    $this.Results = $CategoryResults
+  }
+}
+
+[JsonObject()]
+class EventResults {
+  [JsonProperty(Order = 1)][String]$Name
+  [JsonProperty(Order = 2, NullValueHandling = [NullValueHandling]::Ignore)][DateTime]$Date
+  [JsonProperty(Order = 3)][CategoryResults[]]$Categories
+
+  EventResults($EventName, $EventDate, $CategoryResults) {
+    $this.Name = $EventName
+    $this.Date = $EventDate
+    $this.Categories = $CategoryResults
+  }
+}
+
+[JsonObject()]
+class SeriesResult {
+  [JsonProperty(Order = 3)][String]$Name
+  [JsonProperty(Order = 2)][Int32]$Place
+  [JsonProperty(Order = 1)][Int32]$Points
+
+  SeriesResult([String]$AthleteName, [Int32]$OverallPlace, [Int32]$OverallPoints) {
+    $this.Name = $AthleteName
+    $this.Place = $OverallPlace
+    $this.Points = $OverallPoints
+  }
+}
+
+[JsonObject()]
+class Series {
+  [CategoryResults[]]$Overall
+  [EventResults[]]$Events
+
+  Series([CategoryResults[]]$OverallResults, [EventResults[]]$Events) {
+    $this.Overall = $OverallResults
+    $this.Events = $Events
+  }
+}
 
 function Generate-EventResults {
   param ([String]$EventName, [Object[]]$AthleteResults)
@@ -6,8 +73,7 @@ function Generate-EventResults {
   $categories = $AthleteResults | Group-Object -Property "Category"
 
   foreach ($category in $categories) {
-    $categoryDetails = @{ Name = $category.Name };
-    $categoryDetails.Results = New-Object System.Collections.ArrayList;
+    $categoryResults = New-Object System.Collections.ArrayList;
 
     foreach ($athlete in $category.Group) {
       $points = [Int32](Select-Object -ExpandProperty "$($EventName) - Points" -InputObject $athlete)
@@ -15,24 +81,19 @@ function Generate-EventResults {
         $place = Select-Object -ExpandProperty "$($EventName) - Place" -InputObject $athlete -ErrorAction Ignore
         $time = Select-Object -ExpandProperty "$($EventName) - Time" -InputObject $athlete -ErrorAction Ignore
 
-        $athleteDetail = @{
-          Name = Select-Object -ExpandProperty "Athlete" -InputObject $athlete;
-          Points = $points
-        }
+        $athleteDetail = [AthleteResult]::new(
+          (Select-Object -ExpandProperty "Athlete" -InputObject $athlete),
+          $points,
+          $(if ($place -ne $null) { [Int32]$place } else { $null }),
+          $time
+        )
 
-        if ($place -ne $null) {
-          $athleteDetail.Place = [Int32]$place
-        }
-        if ($time -ne $null) {
-          $athleteDetail.Time = $time
-        }
-
-        [void]$categoryDetails.Results.Add($athleteDetail)
+        [void]$categoryResults.Add($athleteDetail)
       }
     }
 
-    if ($categoryDetails.Results.Count -gt 0) {
-      Write-Output $categoryDetails
+    if ($categoryResults.Count -gt 0) {
+      Write-Output $([CategoryResults]::new($category.Name, ($categoryResults | Sort-Object -Property Place, Time)))
     }
   }
 }
@@ -41,21 +102,17 @@ $csvPath = Resolve-Path $ChallengeResultsCsv
 
 $results = Import-Csv $csvPath
 
-$events = @("Castle to Hanauma", "Mokuleia", "Pineapple Sprint", "Tantalus", "Malaekahana", "Pineapple Hill RR")
-$eventDates = @([DateTime]"2021-03-06", [DateTime]"2021-03-20", [DateTime]"2021-03-27", [DateTime]"2021-04-17", [DateTime]"2021-05-01", [DateTime]"2021-06-12")
+$events = @("Castle to Hanauma", "Mokuleia", "Pineapple Sprint", "Tantalus", "Malaekahana", "Pineapple Hill RR", "DEMRR")
+$eventDates = @([DateTime]"2021-03-06", [DateTime]"2021-03-20", [DateTime]"2021-03-27", [DateTime]"2021-04-17", [DateTime]"2021-05-01", [DateTime]"2021-06-12", [DateTime]"2021-08-29")
 
-$data = @{}
-
-$data.Overall = Generate-EventResults "Overall" $results
-$data.Events = New-Object System.Collections.ArrayList
+$overall = Generate-EventResults "Overall" $results
+$eventObjects = New-Object System.Collections.ArrayList
 
 for ($i = 0; $i -lt $events.Length; $i++) {
   $event = $events[$i]
-  $eventData = @{ Date = $eventDates[$i] }
-  $eventData.Name = $event
-  $eventData.Categories = Generate-EventResults $event $results
+  $eventResults = [EventResults]::new($event, $eventDates[$i], $(Generate-EventResults $event $results))
 
-  [void]$data.Events.Add($eventData)
+  [void]$eventObjects.Add($eventResults)
 }
 
-return $data
+return [Series]::new($overall, $eventObjects)
